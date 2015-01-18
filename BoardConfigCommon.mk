2545e0d4a2e9fb8a78b3efaa7df87f4cae210482
dhacker29 <dhackerdvm@gmail.com>
2015-01-18 16:35:29 -0500

    msm8960_jbbl-common: Remove TARGET_KERNEL_SELINUX_CONFIG
    Change-Id: Id7df2f98ada8531c7b3b8b455ff790aa401bef1d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,7 +41,6 @@ TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8960-common
 TARGET_KERNEL_CONFIG := msm8960_mmi_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := msm8960_mmi_selinux_defconfig
 BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 zcache
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
