008c59029623998818c1f9748ad52ed6bbb55575
nadlabak <pavel@doshaska.net>
2016-03-22 14:06:03 +0100

    init: Adapt for new PowerHAL
    - stop using cpu-boost
    - remove old perf profiles
    - set new sysfs permissions (cpufreq limit, interactive gov)
    
    Change-Id: Ib0cc0bc4328dee7a7dafc2d7e01768de356a6eea

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -119,10 +119,13 @@ case "$target" in
          chown -h system /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
          chown -h system /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
          chown -h system /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+         chown -h system /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis
          chown -h system /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
          chown -h system /sys/devices/system/cpu/cpufreq/interactive/target_loads
          chown -h system /sys/devices/system/cpu/cpufreq/interactive/timer_rate
          chown -h system /sys/devices/system/cpu/cpufreq/interactive/timer_slack
+         chown -h system /sys/kernel/cpufreq_limit/limited_max_freq
+         chown -h system /sys/kernel/cpufreq_limit/limited_min_freq
          echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
          echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
          echo 384000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
