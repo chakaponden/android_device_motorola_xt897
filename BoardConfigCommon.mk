9c3c497fc8a96c97bf4f04714d038b80629bea48
nadlabak <pavel@doshaska.net>
2016-04-08 22:16:33 +0200

    msm8960_jbbl-common: Enable crypto for twrp
    Needed for adopted storage decryption in twrp.
    
    Change-Id: If182a0564be8bb2d0cadfcc5157e9add11e48312

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,7 @@ BOARD_USES_LEGACY_MMAP := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_INCLUDE_CRYPTO := true
 
 # QCOM SELinux policy
 include device/qcom/sepolicy/sepolicy.mk
